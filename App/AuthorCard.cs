45b2026f19bb080723b250553ce8d80b6f436c0d
Chybaka1337 <zommer.03@gmail.com>
2023-04-21 21:14:09 +0300

    The AuthorCard class has been completely redesigned. The SetControls method has been added to work in author editing mode The buttonRemoveClick method was added - it was moved from the main form, for centralized work in one window
    The following methods have been completely redesigned:
    ValidateNameBook, ValidateNameAuthor - their work has been greatly simplified;
    buttonAccept_Click, buttonCancel_Click - their work has also been greatly simplified;
    buttonAccept_Click - now tracks in which mode the form was launched;
    buttonCancel_Click - now just closes the form;

--- App/AuthorCard.cs
+++ App/AuthorCard.cs
@@ -1,140 +1,154 @@
-﻿using System.Text.RegularExpressions;
+﻿using DynamicStructure;
+using System.Text.RegularExpressions;
 
 namespace App
 {
     public partial class AuthorCard : Form
     {
-        static Author author;
-        static MainWindow parrentWindow;
-        string pattern = @"(^[а-яА-Я]{3,12} {1}([а-яА-Я]{5,12} ?){2})";
+        private static Author author;
 
-        public AuthorCard(MainWindow form)
+        private static MainWindow parrentWindow;
+
+        private static Regex regex = new Regex("(^[А-Я]{1}[а-я]{3,11} {1}([А-Я]{1}[а-яА-Я]{3,11} ?){2})");
+
+        private bool editMode = false;
+
+        public AuthorCard(MainWindow form, Author currentAuthor, bool editMode)
         {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
             InitializeComponent();
+            this.editMode = editMode;
+            comboBoxBookTitles.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxBookTitles.DataSourceChanged += comboBoxBookTitles_DataSourceChanged;
+            openFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
 
             parrentWindow = form;
-            parrentWindow.Hide();
+            author = currentAuthor;
 
-            openFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
-            //saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
-
-            buttonConfirmNameBook.Enabled = false;
-            buttonOpenFileDialog.Enabled = false;
-            buttonAccept.Enabled = false;
+            if (editMode) SetControls();
         }
 
-        private void buttonConfirmNameAuthor_Click(object sender, EventArgs e)
+        #region Buttons
+        private void buttonConfirmAuthor_Click(object sender, EventArgs e)
         {
-            if (ValidateNameAuthor(textBoxNameAuthor.Text, buttonConfirmNameAuthor))
+            if (!ValidateNameAuthor(textBoxFullNameAuthor.Text))
             {
-                author = new Author(textBoxNameAuthor.Text);
-                textBoxNameAuthor.Enabled = false;
-                buttonConfirmNameBook.Enabled = true;
-                buttonOpenFileDialog.Enabled = true;
-                buttonAccept.Enabled = true;
+                errorProvider.SetError(sender as Control, "ФИО указано не верно!");
+                return;
             }
+            errorProvider.Clear();
+
+            author.FullName = textBoxFullNameAuthor.Text;
+            SetControls();
         }
 
-        private void buttonConfirmNameBook_Click(object sender, EventArgs e)
+        private void buttonConfirmBookTitle_Click(object sender, EventArgs e)
         {
-            if (ValidateNameBook(textBoxNameBook.Text, buttonConfirmNameBook))
+            if (!ValidateNameBook(textBoxTitleBook.Text))
             {
-                if (author.AddBook(textBoxNameBook.Text))
-                {
-                    textBoxNameBook.Text = string.Empty;
-                    MessageBox.Show("Книга была добавлена!");
-                }
-                else
-                {
-                    MessageBox.Show("Данная книга уже содержится в списке!");
-                }
+                errorProvider.SetError(sender as Control, "Название книги указано не верно!");
+                return;
             }
+
+            if (!author.AddBook(textBoxTitleBook.Text))
+            {
+                textBoxTitleBook.Clear();
+                errorProvider.SetError(sender as Control, "Данная книга уже содержится коллекции!");
+                return;
+            }
+            errorProvider.Clear();
+
+            comboBoxBookTitles.Items.Add(textBoxTitleBook.Text);
+            comboBoxBookTitles_DataSourceChanged(sender, e);
+            textBoxTitleBook.Clear();
+        }
+
+        private void buttonRemoveBook_Click(object sender, EventArgs e)
+        {
+            if (comboBoxBookTitles.SelectedItem == null)
+            {
+                errorProvider.SetError(sender as Control, "Выберите книгу");
+                return;
+            }
+            errorProvider.Clear();
+
+            author.RemoveBook(comboBoxBookTitles.SelectedItem.ToString());
+            comboBoxBookTitles.Items.Remove(comboBoxBookTitles.SelectedItem);
+            comboBoxBookTitles_DataSourceChanged(sender, e);
         }
 
         private void buttonOpenFileDialog_Click(object sender, EventArgs e)
         {
-            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
-            {
-                return;
-            }
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel) return;
 
             var path = openFileDialog.FileName;
-            FileDialog.ReadFromFile(path, author);
+            FileDialog.ReadFromFile(path, author, comboBoxBookTitles);
+
+            comboBoxBookTitles_DataSourceChanged(sender, e);
         }
 
         private void buttonAccept_Click(object sender, EventArgs e)
         {
-            if (author != null)
+            if (!editMode)
             {
                 parrentWindow.comboBoxNameAuthors.Items.Add(author);
                 parrentWindow.comboBoxNameAuthors.Enabled = true;
-                parrentWindow.Show();
-                this.Close();
             }
-            else
-            {
-                MessageBox.Show("Error");
-            }
+            this.Close();
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
         {
-            parrentWindow.Show();
             this.Close();
         }
+        #endregion
 
         #region Validating
-        internal bool ValidateNameAuthor(string name, Control control)
+        private static bool ValidateNameAuthor(string name)
         {
-            if(Regex.IsMatch(name, pattern, RegexOptions.IgnoreCase))
-            {
-                return true;
-            }
-            return false;
-
-            //if (String.IsNullOrWhiteSpace(name))
-            //{
-            //    errorProviderNameAuthor.SetError(control, "Имя не указано!");
-            //    return false;
-            //}
-
-            //foreach (var c in name)
-            //{
-            //    if (!Char.IsLetter(c))
-            //    {
-            //        errorProviderNameAuthor.SetError(control, "Имя не может содержать цифры или спецсимволы!");
-            //        return false;
-            //    }
-            //}
-
-            //if (name.Length < 5 || name.Length > 36)
-            //{
-            //    errorProviderNameAuthor.SetError(control, "Допустимая длина имени от 5 до 36 символов!");
-            //    return false;
-            //}
-            //else
-            //{
-            //    errorProviderNameAuthor.Clear();
-            //    return true;
-            //}
+            return regex.IsMatch(name);
         }
 
-        internal bool ValidateNameBook(string name, Control control)
+        internal static bool ValidateNameBook(string title)
         {
-            if (String.IsNullOrWhiteSpace(name))
+            return !(String.IsNullOrWhiteSpace(title) | title.Length > 36);
+        }
+        #endregion
+
+        #region Controls
+        private void SetControls()
+        {
+            textBoxFullNameAuthor.Text = author.FullName;
+            textBoxFullNameAuthor.Enabled = false;
+            buttonConfirmAuthor.Enabled = false;
+
+            textBoxTitleBook.Enabled = true;
+            buttonConfirmTitleBook.Enabled = true;
+            buttonOpenFileDialog.Enabled = true;
+            buttonAccept.Enabled = true;
+            if (comboBoxBookTitles.Enabled = (author.NumberBooks != 0))
             {
-                errorProviderNameBook.SetError(control, "Название книги не указано!");
-                return false;
+                foreach (var node in author.SortedBooks.EnumeratesNode())
+                {
+                    comboBoxBookTitles.Items.Add(node.Item);
+                }
+                buttonRemoveBook.Enabled = true;
             }
-            else if (name.Length > 36)
+        }
+
+        private void comboBoxBookTitles_DataSourceChanged(object sender, EventArgs e)
+        {
+            if(comboBoxBookTitles.Enabled = (comboBoxBookTitles.Items.Count > 0))
             {
-                errorProviderNameBook.SetError(control, "Допустимая длина от 1 до 36 символов!");
-                return false;
+                comboBoxBookTitles.Update();
+                buttonRemoveBook.Enabled = true;
             }
             else
             {
-                errorProviderNameBook.Clear();
-                return true;
+                comboBoxBookTitles.Update();
+                buttonRemoveBook.Enabled = false;
             }
         }
         #endregion
